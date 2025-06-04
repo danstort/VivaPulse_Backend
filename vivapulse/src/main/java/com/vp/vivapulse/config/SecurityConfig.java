0d8e4cdafea35610ecde5e59bfaeebdaf6c0b7df
David Paredes <davidparedespersonal@gmail.com>
2025-06-04 20:27:57 +0200

    rectificacion

--- vivapulse/src/main/java/com/vp/vivapulse/config/SecurityConfig.java
+++ vivapulse/src/main/java/com/vp/vivapulse/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -56,8 +55,5 @@ public class SecurityConfig {
         return source;
     }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+   
 }

f79580438b28536abb0ba5e28339f55c720c3209
David Paredes <davidparedespersonal@gmail.com>
2025-06-04 16:15:55 +0200

    Refactor security configuration to improve CORS setup in SecurityFilterChain

--- vivapulse/src/main/java/com/vp/vivapulse/config/SecurityConfig.java
+++ vivapulse/src/main/java/com/vp/vivapulse/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF
             .authorizeHttpRequests(auth -> auth
                  .requestMatchers("/api/aliments/**", "/api/trainings/**", "/api/auth/register", "/api/auth/authenticate", "/api/chat").permitAll() // Permitir acceso público
